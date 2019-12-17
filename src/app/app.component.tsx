1fd7349c536d6574502eba41bb85aad67f1c72ce
William Sedlacek <wsedlacekc@gmail.com>
2019-12-17 11:24:30 -0600

    Create React App using --template typescript
    with some tweaks and changes
    
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

---
+++ src/app/app.component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.component.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <span>Hello World</span>
+    </div>
+  );
+};
+
+export { App };

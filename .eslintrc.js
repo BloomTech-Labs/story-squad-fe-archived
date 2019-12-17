6f68a45d4ca4d3578b40ddb50afb578011de9503
William Sedlacek <wsedlacekc@gmail.com>
2019-12-17 14:40:03 -0600

    Cleanup ESLint
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
+        'react-app',
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
+        'prettier/react',
     ],
     parserOptions: {
         ecmaVersion: 2018,
@@ -16,6 +18,9 @@ module.exports = {
     rules: {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        'react/prop-types': 'off',
     },
     settings: {
         react: {

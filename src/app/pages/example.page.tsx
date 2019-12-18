6b130d2eb8d20d63fa25d616ae04394fcd9fe4b1
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-12-18 17:29:54 -0600

    WIP - privacy policy component created and imported to the example page for viewing. Text is in place but even basic formatting is incomplete

--- src/app/pages/example.page.tsx
+++ src/app/pages/example.page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import { ExampleComponent } from '../components/example/example.component';
+import { PrivacyPolicy } from '../components/policies/privacy-policy.component';
 
 const ExamplePage: React.FC = () => {
-    return <ExampleComponent />;
+    return (
+        <>
+            <ExampleComponent />
+            <PrivacyPolicy />
+        </>
+    );
 };
 
 export { ExamplePage };

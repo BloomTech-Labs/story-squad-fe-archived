ace42badf029b3e14ff4a0b8018c2db20e737e3d
ericbanker12 <ericbanker12@gmail.com>
2019-12-19 11:45:01 -0600

    add multi page pdf display to pdf-display component

--- src/app/components/pdf-display.component.tsx
+++ src/app/components/pdf-display.component.tsx
@@ -2,13 +2,33 @@ import React from 'react';
 import { PDFReader } from 'react-read-pdf';
 
 const PdfDisplay: React.FC = () => {
-    const [file, setFile] = React.useState('');
+    const [file, setFile] = React.useState<Buffer>();
+    const [pages, setPages] = React.useState<number[]>([]);
 
     React.useEffect(() => {
         // get pdf as base64 from backend
+        // const pdf =
+        //     'base64 string';
+        // setFile(Buffer.from(pdf, 'base64'));
     }, []);
 
-    return <div>{file && <PDFReader data={Buffer.from(file, 'base64')} />}</div>;
+    const handler = (pages: number) => {
+        // create an array 2, 3, ..., pages
+        const extraPagesArray: number[] = [];
+        for (let i = 2; i <= pages; i++) {
+            extraPagesArray.push(i);
+        }
+        setPages(extraPagesArray);
+    };
+
+    return (
+        <div className='pdf-container'>
+            {file && <PDFReader data={file} page={1} onDocumentComplete={handler} />}
+            {pages.map((page: number) => (
+                <PDFReader data={file} page={page} />
+            ))}
+        </div>
+    );
 };
 
 export { PdfDisplay };

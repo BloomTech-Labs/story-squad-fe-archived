98a12d316bb5ccdc2c3f08566a8c97e67b2e0e1c
William Sedlacek <wsedlacekc@gmail.com>
2019-12-19 15:37:48 -0600

    Finish Sign Up Formatting
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/pages/sign-up.page.tsx
+++ src/app/pages/sign-up.page.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import { Grid, Typography } from '@material-ui/core';
+import {
+    Button,
+    Checkbox,
+    FormControlLabel,
+    Grid,
+    Paper,
+    Tab,
+    Tabs,
+    TextField,
+    Typography,
+    useMediaQuery,
+} from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     sidebar: {
+        paddingTop: theme.spacing(6),
         flexGrow: 1,
         height: '100vh',
         margin: 0,
     },
     tabs: {
-        margin: '40px 52px',
+        paddingRight: theme.spacing(4),
+        paddingLeft: theme.spacing(4),
+
+        [theme.breakpoints.up('md')]: {
+            paddingLeft: theme.spacing(4),
+            paddingRight: theme.spacing(4),
+        },
+        [theme.breakpoints.up('lg')]: {
+            paddingLeft: theme.spacing(10),
+            paddingRight: theme.spacing(10),
+        },
+        borderBottom: '1px solid gray',
     },
-});
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        height: '35vh',
+    },
+    onboarding: {
+        paddingRight: theme.spacing(4),
+        paddingLeft: theme.spacing(4),
+
+        [theme.breakpoints.up('md')]: {
+            paddingLeft: theme.spacing(4),
+            paddingRight: theme.spacing(4),
+        },
+        [theme.breakpoints.up('lg')]: {
+            paddingLeft: theme.spacing(10),
+            paddingRight: theme.spacing(10),
+        },
+
+        marginTop: theme.spacing(12),
+    },
+}));
 
 const SignUpPage: React.FC = () => {
     const classes = useStyles();
+    const theme = useTheme();
+    const md = useMediaQuery(theme.breakpoints.up('md'));
+
     const [value, setValue] = React.useState('sign-up');
 
     const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
@@ -27,7 +71,7 @@ const SignUpPage: React.FC = () => {
 
     return (
         <Grid container direction='row' justify='flex-end' alignItems='stretch'>
-            <Grid item xs={4}>
+            <Grid item xs={md ? 5 : 12}>
                 <Paper className={classes.sidebar}>
                     <Tabs
                         className={classes.tabs}
@@ -35,13 +79,47 @@ const SignUpPage: React.FC = () => {
                         onChange={handleChange}
                         indicatorColor='primary'
                         textColor='primary'
-                        centered
                         variant='fullWidth'>
                         <Tab value='sign-up' label='Sign Up' />
                         <Tab value='sign-in' label='Sign In' />
                     </Tabs>
-                    {value === 'sign-up' && <Typography variant='h3'>Sign Up</Typography>}
-                    {value === 'sign-in' && <Typography variant='h3'>Sign In</Typography>}
+                    {value === 'sign-up' && (
+                        <div className={classes.onboarding}>
+                            <Typography variant='h3' gutterBottom>
+                                Sign Up
+                            </Typography>
+                            <Typography variant='subtitle2'>
+                                Start your child reading stories today!
+                            </Typography>
+                            <form className={classes.form}>
+                                <TextField fullWidth label='Username' />
+                                <TextField fullWidth label='Password' />
+                                <TextField fullWidth label='Confirm Password' />
+                                <div>
+                                    <FormControlLabel
+                                        control={<Checkbox />}
+                                        label='I accept the Terms of Service'
+                                    />
+                                    <FormControlLabel
+                                        control={<Checkbox />}
+                                        label='I accept the Privacy Policy'
+                                    />
+                                </div>
+                                <Button variant='contained' size='large'>
+                                    Sign In
+                                </Button>
+                            </form>
+                        </div>
+                    )}
+                    {value === 'sign-in' && (
+                        <div className={classes.onboarding}>
+                            <Typography variant='h4'>Sign In</Typography>
+                            <form className={classes.form}>
+                                <TextField fullWidth label='Username' />
+                                <TextField fullWidth label='Password' />
+                            </form>
+                        </div>
+                    )}
                 </Paper>
             </Grid>
         </Grid>

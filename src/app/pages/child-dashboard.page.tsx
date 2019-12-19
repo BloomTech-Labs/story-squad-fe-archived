b46ebf794f522e9294ecbfe824b3704368b3dcd0
ericbanker12 <ericbanker12@gmail.com>
2019-12-18 18:13:04 -0600

    create child dashboard page

---
+++ src/app/pages/child-dashboard.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChildDashboard: React.FC = () => {
+    return (
+        <div>
+            {/* link to current chapter */}
+        </div>
+    );
+};
+
+export { ChildDashboard };

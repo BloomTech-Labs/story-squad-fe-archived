926ce04ad10a8eb4d6bcb349540da155518dfaa1
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-06 15:56:26 -0600

    add axios get to children-list

--- src/app/components/dashboard/children-list.component.tsx
+++ src/app/components/dashboard/children-list.component.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
 import { ChildCard } from './child-card.component';
+
 
 const ChildrenList: React.FC = () => {
     const [children, setChildren] = React.useState<any[]>([]);
 
     React.useEffect(() => {
-        // get children from backend
+        axios
+        .get(localhost:4000/children)
+        .then(res => {
+            setChildren(res.data)
+        })
     }, []);
 
     return (
         <div>
             {children.map((child) => (
-                <ChildCard child={child} />
+                <ChildCard key={child.id} child={child} />
             ))}
         </div>
     );

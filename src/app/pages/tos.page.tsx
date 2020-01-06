b42058bca400cf23db2cf7f650a4e1f4c43c2f6b
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-06 11:21:45 -0600

    added tos component and page

---
+++ src/app/pages/tos.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import { ToS } from '../components/policies/tos.component';
+
+
+const ToSPage: React.FC = () => {
+    return (
+        <>
+            <ToS />
+        </>
+    );
+};
+
+export { PrivacyPage };

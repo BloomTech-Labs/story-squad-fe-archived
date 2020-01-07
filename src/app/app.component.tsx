38efcb965c2df6efdd9e44406a99d11fa8b7f47c
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-01-07 15:09:56 -0600

    started child page

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -6,6 +6,7 @@ import { PrivateRoute } from './components/common/private-route.component';
 
 import { ExamplePage } from './pages/example.page';
 import { Private } from './pages/private.page';
+import { ChildCard } from './components/dashboard/child-card.component';
 
 const App: React.FC = () => {
     return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
             <Switch>
                 <PrivateRoute redirect='/' path='/private' component={Private} />
                 <Route path='/' component={ExamplePage} />
+                <Route path='/child' component={ChildCard} />
             </Switch>
         </Router>
     );
---
+++ src/app/pages/child.page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ChildCard } from '../components/dashboard/child-card.component';
+
+const ChildPage: React.FC = () => {
+    return <ChildCard/>;
+};
+
+export { ChildPage };

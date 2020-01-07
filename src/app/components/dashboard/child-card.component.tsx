337b3360d676657ad20903cf6c8e96f34f5a3aec
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-01-07 16:31:32 -0600

    commiting changes to be able to pull from master.

--- src/app/components/dashboard/child-card.component.tsx
+++ src/app/components/dashboard/child-card.component.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
 import { Child } from '../../models';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    card: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+
+  export default function SimpleCard() {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+  
 
 interface ChildCardProps {
     child: Child;
@@ -9,11 +37,29 @@ const ChildCard: React.FC<ChildCardProps> = (props) => {
     const child = props.child;
 
     return (
-        <div>
-            <h2>{child.username}</h2>
-            <button>preferences</button>
-        </div>
+        <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
     );
 };
-
+  }
 export { ChildCard };
+  

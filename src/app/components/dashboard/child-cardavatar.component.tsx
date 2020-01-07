632488f8eb4817a65ed31cd7ac244bcce3cf5728
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-01-07 17:10:38 -0600

    tried adding image to card as avatar.
    Not rendering.

---
+++ src/app/components/dashboard/child-cardavatar.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// nodejs library that concatenates classes
+import classNames from 'classnames';
+// nodejs library to set properties for components
+// import PropTypes from "prop-types";
+// @material-ui/core components
+import withStyles from '@material-ui/core/styles/withStyles';
+// @material-ui/icons
+// core components
+
+
+
+function CardAvatar({ ...props }: any) {
+  const { classes, children, className, plain, profile, ...rest } = props;
+  const cardAvatarClasses = classNames({
+    [classes.cardAvatar]: true,
+    [classes.cardAvatarProfile]: profile,
+    [classes.cardAvatarPlain]: plain,
+    [className]: className !== undefined
+  });
+  return (
+    <div className={cardAvatarClasses} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+
+export default withStyles(CardAvatar);

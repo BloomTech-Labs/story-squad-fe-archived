6aa172892025fb02be050cc4d45456528e269b32
Britt Moyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-07 10:31:41 -0600

    Privacy policy and Terms of Service (#4)
    * WIP - privacy policy component created and imported to the example page for viewing. Text is in place but even basic formatting is incomplete
    
    * basic formatting implemented on privacy policy component.
    
    * updated content as per Graig
    
    * fixed privacy-policy component. Moved component to its own page
    
    * added tos component and page
    
    * fix a couple typos
    
    * add basic tests to privacy policy components
    
    * add routes to privacy and ToS pages
    
    * Fix Netlify Router
    
    * Reduce Changes
    
    Co-authored-by: Eric Banker <EricBanker12@users.noreply.github.com>
    Co-authored-by: William Sedlacek <wsedlacekc@gmail.com>

---
+++ src/app/pages/privacy-policy.page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import { PrivacyPolicy } from '../components/policies/privacy-policy.component';
+
+
+const PrivacyPage: React.FC = () => {
+    return (
+        <>
+            <PrivacyPolicy />
+        </>
+    );
+};
+
+export { PrivacyPage };

da7f752cf0605541d751badfa324bd7b1be73a89
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-01-08 09:19:23 -0800

    Commented out mock testing for futute use

--- src/app/hooks/api/api.hook.spec.tsx
+++ src/app/hooks/api/api.hook.spec.tsx
@@ -9,43 +9,43 @@ interface Todo {
     completed: boolean;
 }
 
-afterEach(() => {
-    // cleaning up the mess left behind the previous test
-    mockAxios.reset();
-});
-
-it('useAPI should get data from the server and convert it to axios', () => {
-    const catchFn = jest.fn(),
-        thenFn = jest.fn();
-
-    // using the component, which should make a server response
-    const clientMessage = 'client is saying hello!';
-
-    useAPI(clientMessage);
-    // .then(thenFn)
-    // .catch(catchFn);
-
-    // since `post` method is a spy, we can check if the server request was correct
-    // a) the correct method was used (post)
-    // b) went to the correct web service URL ('/web-service-url/')
-    // c) if the payload was correct ('client is saying hello!')
-    expect(mockAxios.post).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com', {
-        data: clientMessage,
-    });
-
-    // simulating a server response
-    const responseObj = { data: 'server says hello!' };
-    mockAxios.mockResponse(responseObj);
-
-    // checking the `then` spy has been called and if the
-    // response from the server was converted to upper case
-    expect(thenFn).toHaveBeenCalledWith('SERVER SAYS HELLO!');
-
-    // catch should not have been called
-    expect(catchFn).not.toHaveBeenCalled();
-});
-
 describe('useAPI()', () => {
+    // afterEach(() => {
+    //     // cleaning up the mess left behind the previous test
+    //     mockAxios.reset();
+    // });
+
+    // it('useAPI should mock data and convert it to axios', () => {
+    //     const catchFn = jest.fn(),
+    //         thenFn = jest.fn();
+
+    //     // using the component, which should make a server response
+    //     const clientMessage = 'client is saying hello!';
+
+    //     useAPI(clientMessage);
+    //     // .then(thenFn)
+    //     // .catch(catchFn);
+
+    //     // since `post` method is a spy, we can check if the server request was correct
+    //     // a) the correct method was used (post)
+    //     // b) went to the correct web service URL ('/web-service-url/')
+    //     // c) if the payload was correct ('client is saying hello!')
+    //     expect(mockAxios.post).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com', {
+    //         data: clientMessage,
+    //     });
+
+    //     // simulating a server response
+    //     const responseObj = { data: 'server says hello!' };
+    //     mockAxios.mockResponse(responseObj);
+
+    //     // checking the `then` spy has been called and if the
+    //     // response from the server was converted to upper case
+    //     expect(thenFn).toHaveBeenCalledWith('SERVER SAYS HELLO!');
+
+    //     // catch should not have been called
+    //     expect(catchFn).not.toHaveBeenCalled();
+    // });
+
     it('should GET data by default', async () => {
         const { result } = renderHook(() => useAPI<Todo>(`${TEST_URL}/todos/1`));
 

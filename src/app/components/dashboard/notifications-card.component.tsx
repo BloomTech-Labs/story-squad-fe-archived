effdd5329c425cdcb06584bcd641b948ff713010
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-01-08 11:49:12 -0600

    notifications card not functional

---
+++ src/app/components/dashboard/notifications-card.component.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Child } from '../../models';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles({
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
+interface NotificationsCardProps {
+    child: Child;
+}
+
+const ChildCard: React.FC<NotificationsCardProps> = (props) => {
+    const classes = useStyles();
+    const child = props.child;
+
+    return (
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography variant='h5' component='h2'>
+                   Notifications Center
+                </Typography>
+                <List>
+          {['1/8', '1/7', '1/7', '1/7'].map((text, index) => (
+            <ListItem button key={text}>
+             <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Button>View Account</Button>
+            </CardContent>
+        </Card>
+    );
+};
+
+export { NotificationsCard };

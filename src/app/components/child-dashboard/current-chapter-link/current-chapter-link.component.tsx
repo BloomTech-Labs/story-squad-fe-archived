cedcb49d11d8617a91057bc5ad92362e7c72fed0
ericbanker12 <ericbanker12@gmail.com>
2020-01-09 15:36:09 -0600

    add test to currentChapterLink component, add WIP axios call to currentChapterLink component

--- src/app/components/child-dashboard/current-chapter-link/current-chapter-link.component.tsx
+++ src/app/components/child-dashboard/current-chapter-link/current-chapter-link.component.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
+import requestFactory from '../../../util/requestFactory';
+
 const CurrentChapterLink: React.FC = (props) => {
-    const [week, setWeek] = React.useState(1);
+    const [week, setWeek] = React.useState<number>();
+
+    const axios = requestFactory();
 
     React.useEffect(() => {
         // look up week # for child
-    }, []);
+        if (!week) {
+            setWeek(1); // temporarily hardcoded until child-account-management is merged
+
+            // axios
+            //     .get(`/children/me`)
+            //     .then((res) => {
+            //         // console.log(res.data);
+            //         if (res && res.data) {
+            //             setWeek(res.data.week);
+            //         }
+            //     })
+            //     .catch((err) => {
+            //         console.error(err);
+            //     });
+        }
+    }, [axios, week]);
+
+    if (!week) return <span>Loading...</span>;
 
     return <a href={`/story/${week}`}>Current Story</a>;
 };

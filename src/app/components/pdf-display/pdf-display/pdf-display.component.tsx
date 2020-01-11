ba7fe38da958290eab3cb66c9e5af9745cd87b3d
William Sedlacek <wsedlacekc@gmail.com>
2020-01-10 19:39:35 -0600

    Cleanup
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/pdf-display/pdf-display/pdf-display.component.tsx
+++ src/app/components/pdf-display/pdf-display/pdf-display.component.tsx
@@ -1,6 +1,7 @@
-import './pdf-display.component.scss';
 import React from 'react';
 import { PDFReader } from 'react-read-pdf-b';
+
+import { makeStyles } from '@material-ui/core/styles';
 
 import { useAPI } from '../../../hooks';
 import { numbersBetweenZero } from '../../../util';
@@ -9,11 +10,24 @@ import * as pdfjsLib from 'pdfjs-dist';
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
 pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
+const useStyles = makeStyles(() => ({
+    root: {
+        'display': 'flex',
+        'flexDirection': 'column',
+        'justifyContent': 'flex-start',
+        'alignItems': 'center',
+        '& > canvas': {
+            maxWidth: '100%',
+        },
+    },
+}));
+
 interface PDFDisplayProps {
     week: string | number;
 }
 
 const PDFDisplay: React.FC<PDFDisplayProps> = ({ week }) => {
+    const classes = useStyles();
     const [file, setFile] = React.useState<Buffer>();
     const [pages, setPages] = React.useState<number[]>();
     const { request, response } = useAPI(`/canon/${week}`);
@@ -29,14 +43,14 @@ const PDFDisplay: React.FC<PDFDisplayProps> = ({ week }) => {
 
     React.useEffect(() => {
         if (!file) return;
-        const loadingTask = pdfjsLib.getDocument({ data: file });
-        loadingTask.promise.then(({ numPages }) => setPages(numbersBetweenZero(numPages)));
+        const { promise } = pdfjsLib.getDocument({ data: file });
+        promise.then(({ numPages }) => setPages(numbersBetweenZero(numPages)));
     }, [file]);
 
     if (!file) return <div>Downloading...</div>;
     if (!pages) return <div>Loading...</div>;
     return (
-        <div className='pdf-container'>
+        <div className={classes.root}>
             {pages.map((page: number) => (
                 <PDFReader key={page} data={file} page={page} scale={1.5} />
             ))}

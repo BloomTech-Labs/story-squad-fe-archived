0e86f6927cf464d4ab7dd1837494f1a7330a0094
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-14 11:16:20 -0600

    created dummy component for import, WIP debugging TS error

--- src/app/components/dashboard/stripe/subscription/add-subscription.component.tsx
+++ src/app/components/dashboard/stripe/subscription/add-subscription.component.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-const stripe = require('stripe')('pk_test_JF5xfRvewfyAeFPgugZCgIxM00sGTTerR8');
+const AddSubscription: React.FC = ()=> {
+    return (
+        null
+    )
+}
 
-stripe.subscriptions.create(
-  {
-    customer: 'cus_GXybZYX6gR0XsJ',
-    items: [{plan: 'plan_GVQ796LiwZugJ9'}],
-  },
-  function(err, subscription) {
-    // asynchronously called
-  }
-);
+export { AddSubscription }

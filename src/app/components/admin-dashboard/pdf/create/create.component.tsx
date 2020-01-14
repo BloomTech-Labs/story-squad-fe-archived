486607c2fe006f2839f3088445086b5e7624b77b
ericbanker12 <ericbanker12@gmail.com>
2020-01-14 14:53:18 -0600

    PdfCreate, add handler for file upload, add handler for onSubmit, add file input labels

--- src/app/components/admin-dashboard/pdf/create/create.component.tsx
+++ src/app/components/admin-dashboard/pdf/create/create.component.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import { useAPI } from '../../../../hooks';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, Input, InputLabel } from '@material-ui/core';
 import { useHistory } from 'react-router';
 
 const PdfCreate: React.FC = () => {
     const history = useHistory();
     const { request, response } = useAPI('/canon', 'POST');
-
     const [state, setState] = React.useState({ week: '', pdf: '', pdfAlt: '' });
+
+    const { week, pdf, pdfAlt } = state;
 
     const handleInputChange = (key: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
         setState({ ...state, [key]: e.target.value });
@@ -19,26 +20,23 @@ const PdfCreate: React.FC = () => {
             const reader = new FileReader();
             reader.readAsDataURL(e.target.files[0]);
             reader.onload = (e) => {
-                // console.log(`hello ${e.target?.result?.toString()}`);
+                const base64 = e.target?.result?.toString().replace(/^.*base64,/, '');
+                setState({ ...state, [key]: base64 });
             };
         }
-        // setState({ ...state, [key]:  });
     };
 
-    const handleChange = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        // TODO edit data shape/type
-        // TODO send data
-        // request()
+        request({ ...state, week: Number(week) });
     };
 
     React.useEffect(() => {
         if (response) history.push('/admin');
     }, [history, response]);
 
-    const { week, pdf, pdfAlt } = state;
     return (
-        <form onSubmit={handleChange}>
+        <form onSubmit={handleSubmit}>
             <TextField
                 type='number'
                 label='Week'
@@ -47,14 +45,21 @@ const PdfCreate: React.FC = () => {
                 onChange={handleInputChange('week')}
             />
 
-            <TextField
+            <InputLabel htmlFor='pdf'>Default PDF</InputLabel>
+            <Input
+                id='pdf'
                 type='file'
-                label='PDF'
                 inputProps={{ accept: '.pdf' }}
                 required
-                // value={pdf}
-                // onChange={handleInputChange('pdf')}
                 onChange={handleFileChange('pdf')}
+            />
+
+            <InputLabel htmlFor='pdfAlt'>Dyslexic PDF</InputLabel>
+            <Input
+                id='pdfAlt'
+                type='file'
+                inputProps={{ accept: '.pdf' }}
+                onChange={handleFileChange('pdfAlt')}
             />
 
             <Button type='submit' variant='contained' color='primary'>

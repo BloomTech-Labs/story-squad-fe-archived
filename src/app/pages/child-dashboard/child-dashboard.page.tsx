f4d2c5d65bdb23de2e1eb8ddc28a2ad4af5da544
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-01-14 16:20:58 -0600

    added app bar -  kids dashboard

--- src/app/pages/child-dashboard/child-dashboard.page.tsx
+++ src/app/pages/child-dashboard/child-dashboard.page.tsx
@@ -6,18 +6,58 @@ import {
     SimpleBottomNavigation,
     FanFictionUpload,
 } from '../../components';
+import { Button, AppBar, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { Child } from '../../models';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        width: `calc(100% - 240px)`,
+        marginLeft: 240,
+    },
+    spacer: {
+        flex: '1',
+    },
+    main: {
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3),
+    },
+    toolbar: theme.mixins.toolbar,
+}));
+
 const ChildDashboard: React.FC = () => {
+    const classes = useStyles();
+    const logout = () => window.dispatchEvent(new Event('logout'));
     return (
-        <div>
-            <WelcomeCard />
-            <br></br>
-            <CurrentChapterLink />
-            <br></br>
-            <FanFictionUpload />
-            <br></br>
-            <SimpleBottomNavigation />
+        <div className={classes.root}>
+            <AppBar position='fixed' className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant='h6' noWrap>
+                        Kid's Dashboard
+                    </Typography>
+                    <div className={classes.spacer} />
+                    <Button onClick={logout} color='inherit'>
+                        Logout
+                    </Button>
+                </Toolbar>
+            </AppBar>
+
+            <main className={classes.main}>
+                <div className={classes.toolbar} />
+                <WelcomeCard />
+                <br></br>
+                <CurrentChapterLink />
+                <br></br>
+                <FanFictionUpload />
+                <br></br>
+                <SimpleBottomNavigation />
+            </main>
         </div>
     );
 };

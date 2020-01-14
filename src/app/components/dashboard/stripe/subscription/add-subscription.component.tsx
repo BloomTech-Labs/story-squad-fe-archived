fec705d82d3770e6e10330fe85ef99718780d47b
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-14 11:03:42 -0600

    created basic subscription page

--- src/app/components/dashboard/stripe/subscription/add-subscription.component.tsx
+++ src/app/components/dashboard/stripe/subscription/add-subscription.component.tsx
@@ -1,1 +1,13 @@
 import React from 'react'
+
+const stripe = require('stripe')('pk_test_JF5xfRvewfyAeFPgugZCgIxM00sGTTerR8');
+
+stripe.subscriptions.create(
+  {
+    customer: 'cus_GXybZYX6gR0XsJ',
+    items: [{plan: 'plan_GVQ796LiwZugJ9'}],
+  },
+  function(err, subscription) {
+    // asynchronously called
+  }
+);
--- src/app/pages/dashboard/payment/subscribe/subscribe.page.tsx
+++ src/app/pages/dashboard/payment/subscribe/subscribe.page.tsx
@@ -1,1 +1,16 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom';
+import { StripeWrapper, AddSubscription } from '../../../../components'
+
+const SubscribePage: React.FC = () => {
+    const history = useHistory();
+    const redirect = () => history.push('/dashboard');
+
+    return(
+        <StripeWrapper>
+            <AddSubscription onComplete={redirect} />
+        </StripeWrapper>
+    )
+}
+
+export { SubscribePage }

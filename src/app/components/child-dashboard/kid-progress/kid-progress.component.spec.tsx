a8283a54af87f11de0e4baa94d4a0db4108f1a58
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-01-14 17:57:10 -0600

    dummy test for kid-progress

---
+++ src/app/components/child-dashboard/kid-progress/kid-progress.component.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { KidProgressCard } from './kid-progress.component';
+
+describe('KidProgressCard', () => {
+    it('renders without errors', () => {
+        const child = { id: 1, username: 'Sam', grade: 3 };
+        const { baseElement } = render(
+            <BrowserRouter>
+                <KidProgressCard child={child} />
+            </BrowserRouter>
+        );
+        expect(baseElement).toBeInTheDocument();
+    });
+});

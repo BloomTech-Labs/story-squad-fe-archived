30f6512cc2f88922a80133635ca236458bdd5172
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-15 13:03:47 -0600

    WIP on subscription form

--- src/app/components/dashboard/stripe/subscription/add-subscription.component.tsx
+++ src/app/components/dashboard/stripe/subscription/add-subscription.component.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Button, RadioGroup } from '@material-ui/core';
+import { Button, Radio, RadioGroup, FormLabel, FormControlLabel } from '@material-ui/core';
 import { ReactStripeElements, injectStripe } from 'react-stripe-elements';
 import { useHistory } from 'react-router';
-import { useAPI } from '../../../../hooks';
+import { useAPI, useForm } from '../../../../hooks';
 interface AddSubscriptionProps {
     onComplete?: () => void;
     childID: {};
@@ -12,14 +12,35 @@ const _AddSubscription: React.FC<AddSubscriptionProps &
     ReactStripeElements.InjectedStripeProps> = ({ stripe, onComplete, childID }) => {
     const history = useHistory();
     const { request, response } = useAPI('/payment/subscribe', 'POST');
+    const { state, handleInputChange, handleSubmitBuilder } = useForm({
+        child: childID,
+        plan: '',
+    });
+
+    const handleChange = handleSubmitBuilder(request);
     const redirectButton = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         history.push('/dashboard');
+
+        React.useEffect(() => {
+            if (response) history.push('/dashboard');
+        }, []);
     };
-    console.log('props', childID);
     return (
-        <form onSubmit={redirectButton}>
-            <RadioGroup></RadioGroup>
+        <form onSubmit={handleChange}>
+            <FormLabel>Subscription Options</FormLabel>
+            <RadioGroup defaultValue='plan_GVQ796LiwZugJ9' name='plans'>
+                <FormControlLabel
+                    value='plan_GVQ796LiwZugJ9'
+                    control={<Radio />}
+                    label='Test Subscription - $1/month'
+                />
+                <FormControlLabel
+                    value=''
+                    control={<Radio />}
+                    label='I do not wish to subscribe at this time'
+                />
+            </RadioGroup>
 
             <Button type='submit' color='primary'>
                 Subscribe

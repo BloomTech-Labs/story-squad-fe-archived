1d8bb474f847dee2076ba231478f4a06edb2460c
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-01-16 09:42:41 -0800

    Added slide animation smile image to error page

--- src/app/pages/error-page/error-page.tsx
+++ src/app/pages/error-page/error-page.tsx
@@ -6,16 +6,20 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { findByLabelText } from '@testing-library/dom';
 
 const ErrorPage = () => {
     const history = useHistory();
     return (
         <div id='error'>
-            <h1 className='notFoundTitle'>Oops... Don't Worry</h1>
-            <p className='notFoundDesc'>
+            <h1 className='notFoundTitle'>OOPS...DON'T WORRY</h1>
+            <div className='animation'>
+                <SimpleSlide />
+            </div>
+            <h5 className='notFoundDesc'>
                 The page you're looking for isn't available, but we'll help you continue your
                 mission!
-            </p>
+            </h5>
             <div>
                 <Button onClick={history.goBack}>Let's go back</Button>
             </div>
@@ -26,27 +30,16 @@ const ErrorPage = () => {
 const useStyles = makeStyles((theme) => ({
     root: {
         height: 180,
+        float: 'right',
     },
     wrapper: {
-        width: 100 + theme.spacing(2),
-    },
-    paper: {
-        zIndex: 1,
-        position: 'relative',
-        margin: theme.spacing(1),
-    },
-    svg: {
-        width: 100,
-        height: 100,
-    },
-    polygon: {
-        fill: theme.palette.common.white,
-        stroke: theme.palette.divider,
-        strokeWidth: 1,
+        width: 400 + theme.spacing(2),
+        display: 'flex',
+        alignContent: 'flexEnd',
     },
 }));
 
-function SimpleSlide() {
+export default function SimpleSlide() {
     const classes = useStyles();
     const [checked, setChecked] = React.useState(false);
 
@@ -56,16 +49,8 @@ function SimpleSlide() {
     return (
         <div className={classes.root}>
             <div className={classes.wrapper}>
-                <FormControlLabel
-                    control={<Switch checked={checked} onChange={handleChange} />}
-                    label='Show'
-                />
-                <Slide direction='up' in={checked} mountOnEnter unmountOnExit>
-                    <Paper elevation={4} className={classes.paper}>
-                        <svg className={classes.svg}>
-                            <polygon points='0,100 50,00, 100,100' className={classes.polygon} />
-                        </svg>
-                    </Paper>
+                <Slide direction='left' in={true} mountOnEnter unmountOnExit>
+                    <img src={`${process.env.PUBLIC_URL}/assets/smile.png`} />
                 </Slide>
             </div>
         </div>

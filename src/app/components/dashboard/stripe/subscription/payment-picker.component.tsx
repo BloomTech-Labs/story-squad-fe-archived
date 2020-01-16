9d3360b867aee5d5c25f34c054f3c69bfc4f674e
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2020-01-16 15:57:53 -0600

    payment picker toggles based on whether card already exists

--- src/app/components/dashboard/stripe/subscription/payment-picker.component.tsx
+++ src/app/components/dashboard/stripe/subscription/payment-picker.component.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { StripeWrapper, CardForm } from '../../../../components';
+import { CardList } from '../card-list/card-list.component';
+import { useAPI } from '../../../../hooks';
+import { CreditCard } from '../../../../models';
 
 const PaymentPicker: React.FC = () => {
-    //if parent doesn't have a card
-    return (
-        <StripeWrapper>
-            <CardForm />
-        </StripeWrapper>
-    );
+    //check parent
+    const { request, response } = useAPI<{ cards: CreditCard[] }>('/payment/cards');
+
+    React.useEffect(() => {
+        request();
+    }, [request]);
+
+    if (!response) {
+        //if parent doesn't have a card
+        return (
+            <StripeWrapper>
+                <CardForm />
+            </StripeWrapper>
+        );
+    } else {
+        //if parent has a card
+        return <CardList />;
+    }
 };
 
 export { PaymentPicker };

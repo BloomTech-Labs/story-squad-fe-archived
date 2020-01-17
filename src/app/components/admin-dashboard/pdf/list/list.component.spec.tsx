c072a0fe76872aeb1083da751d6c6a1085338c21
ericbanker12 <ericbanker12@gmail.com>
2020-01-17 14:33:45 -0600

    create render tests for all new components

--- src/app/components/admin-dashboard/pdf/list/list.component.spec.tsx
+++ src/app/components/admin-dashboard/pdf/list/list.component.spec.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
-import { AccountsList } from './list.component';
+import { PdfList } from './list.component';
 
 jest.mock('../../../../hooks', () => ({
     useAPI: () => ({
         request: () => {},
         response: {
-            admin: [
-                { id: 1, email: 'test', role: 'admin', validpass: true },
-                { id: 2, email: 'test2', role: 'admin', validpass: true },
+            canon: [
+                { week: 1, base64: 'test' },
+                { week: 2, base64: 'test2' },
             ],
         },
     }),
 }));
 
-describe('AccountsList', () => {
+describe('PdfList', () => {
     it('renders without errors', () => {
         const { baseElement } = render(
             <Router>
-                <AccountsList />
+                <PdfList />
             </Router>
         );
         expect(baseElement).toBeInTheDocument();

54c65d6a319a665d5f9913ce491ea6553930763f
William Sedlacek <wsedlacekc@gmail.com>
2020-01-21 17:15:53 -0600

    Fix Testing
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/dashboard/stripe/payment-picker/payment-picker.component.spec.tsx
+++ src/app/components/dashboard/stripe/payment-picker/payment-picker.component.spec.tsx
@@ -1,1 +1,11 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+
+import { PaymentPicker } from './payment-picker.component';
+
+describe('Payment Picker', () => {
+    it('renders without errors', () => {
+        const { baseElement } = render(<PaymentPicker />);
+        expect(baseElement).toBeInTheDocument();
+    });
+});
--- src/app/pages/dashboard/payment/subscribe/subscribe.page.spec.tsx
+++ src/app/pages/dashboard/payment/subscribe/subscribe.page.spec.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ChildListProvider } from '../../../../state';
 import { SubscribePage } from './subscribe.page';
 
 describe('SubscribePage', () => {
     it('renders without errors', () => {
         const { baseElement } = render(
             <BrowserRouter>
-                <SubscribePage />
+                <ChildListProvider>
+                    <SubscribePage />
+                </ChildListProvider>
             </BrowserRouter>
         );
         expect(baseElement).toBeInTheDocument();

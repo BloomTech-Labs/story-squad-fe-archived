d375dfae1cfb05f783a0561ef42fb5a7efe357a6
William Sedlacek <wsedlacekc@gmail.com>
2020-01-24 12:07:14 -0600

    Fix Exports
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/components/child-dashboard/index.ts
+++ src/app/components/child-dashboard/index.ts
@@ -1,4 +1,3 @@
 export * from './current-chapter-link/current-chapter-link.component';
 export * from './welcome-card/welcome-card.component';
-export * from './fan-fiction-upload/fan-fiction-upload.component';
 export * from './kid-progress/kid-progress.component';

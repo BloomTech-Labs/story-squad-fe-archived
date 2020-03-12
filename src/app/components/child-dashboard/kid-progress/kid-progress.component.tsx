6dc88c3ea6e03d95648c109775adfb7248f0effb
Allie Robinson <allierob93@gmail.com>
2020-03-12 11:30:57 -0500

    changed mission colors from blue/orange to black/white

--- src/app/components/child-dashboard/kid-progress/kid-progress.component.tsx
+++ src/app/components/child-dashboard/kid-progress/kid-progress.component.tsx
@@ -135,7 +135,8 @@ const useStyles = makeStyles((theme) => ({
         'fontSize': '86px',
         'fontWeight': 'bold',
         '-webkit-text-stroke-width': '1px',
-        '-webkit-text-stroke-color': '#ff6d3a',
+        '-webkit-text-stroke-color': '#000000',
+        'color': '#ffffff',
     },
     headerBorder: {
         border: '4px solid #292929',

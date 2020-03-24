3837a9e9a674b1c4377af0195a9d624975d201a5
Mashima Button <52222646+zimashima@users.noreply.github.com>
2020-03-24 16:09:29 -0400

    Update points (#59)
    * added modal progress in case if undefined get rendered first, render student and teammate story and submission
    
    * Team button (#60)
    
    * removed welcome, adjusted styles for size
    
    * fixed avatar postions
    
    * avatar and username tweaks
    
    * story form changes
    
    * progress update fix
    
    * possible submit fix
    
    * possible submit fix
    
    * drawing form fixes
    
    * both forms functional
    
    * removed dependancy babel
    
    * Unneccessary component as of rn
    
    * removed comments, unecessary code
    
    * team up button fix
    
    Co-authored-by: squashgray <squashgray@gmail.com>
    Co-authored-by: Allie Robinson <48700332+allie-rae@users.noreply.github.com>
    
    Co-authored-by: squashgray <squashgray@gmail.com>
    Co-authored-by: Allie Robinson <48700332+allie-rae@users.noreply.github.com>

---
+++ src/app/components/child-dashboard/creative-content-submission/story-form/styles.ts
@@ -0,0 +1,82 @@
+import { makeStyles } from '@material-ui/core/styles';
+import cityscape from '../../../../pages/child-dashboard/icons/cityscape.png';
+export const useStyles = makeStyles((theme) => ({
+    header: {
+        color: theme.palette.primary.contrastText,
+        backgroundColor: theme.palette.primary.main,
+        borderBottom: '7px solid #000000',
+    },
+    card: {
+        borderLeft: '7px solid #000000',
+        borderBottom: '7px solid #000000',
+        borderRight: '7px solid #000000',
+    },
+    content: {
+        display: 'grid',
+        gridGap: theme.spacing(3),
+    },
+    form: {
+        margin: '0 auto',
+        width: '50%',
+    },
+    preview: {
+        height: 200,
+        width: 200,
+    },
+    wrapper: {
+        margin: theme.spacing(1),
+        position: 'fixed',
+        bottom: theme.spacing(2),
+        right: theme.spacing(2),
+    },
+    orangeButton: {
+        'backgroundColor': '#FF6B35',
+        'fontSize': '24px',
+        'fontWeight': 'bold',
+        'borderRadius': '10px',
+        'color': 'white',
+        'width': '200px',
+
+        'border': '3px solid #292929',
+        'textTransform': 'capitalize',
+        'fontFamily': 'nunito',
+        '&:hover': {
+            backgroundColor: '#FF6B35',
+        },
+    },
+    buttonText: {
+        fontSize: '24px',
+        fontWeight: 'bold',
+        fontFamily: 'nunito',
+    },
+    appBar: {
+        height: '229px',
+        backgroundColor: '#6CEAE6',
+        backgroundImage: `url(${cityscape})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '101% 103%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottom: '7px solid #000000',
+        borderTop: '7px solid #000000',
+    },
+    headerFont: {
+        'fontFamily': 'Bangers',
+        'color': '#FFFFFF',
+        'fontSize': '86px',
+        'fontWeight': 'bold',
+        '-webkit-text-stroke-width': '1px',
+        '-webkit-text-stroke-color': '#000000',
+    },
+    buttons: {
+        display: 'flex',
+        justifyContent: 'space-between',
+    },
+    promptText: {
+        fontFamily: 'nunito',
+        fontSize: '36px',
+        fontWeight: 'normal',
+        color: '#292929',
+    },
+}));

654c3a700487b1a4cf784aaecff747657eed3748
Ben <bigriffin94@outlook.com>
2020-04-18 13:16:45 -0700

    pulling from origin

--- src/app/components/versus/versus.component.tsx
+++ src/app/components/versus/versus.component.tsx
@@ -4,29 +4,7 @@ import ava1 from './img/ava1.png';
 import ava2 from './img/ava2.png';
 import ava3 from './img/ava3.png';
 import ava4 from './img/ava4.png';
-import {
-    Button,
-    Avatar,
-    Typography,
-    Container,
-    Grid,
-    Modal,
-    Fade,
-    Backdrop,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    CircularProgress,
-    Card,
-    Checkbox,
-    Paper,
-    Popper,
-    MenuItem,
-    MenuList,
-    Grow,
-    ClickAwayListener,
-} from '@material-ui/core';
+import { Button, Avatar, Typography, Container, Grid } from '@material-ui/core';
 import { useStyles } from './versus-styles';
 import { Child } from '../../models';
 import { Link } from 'react-router-dom';
@@ -127,48 +105,7 @@ const Versus: React.FC<VersusProps> = ({ thisBattle }) => {
         <Container className={classes.containerStyling}>
             <VersusHeader homeTeam={homeTeamNames} awayTeam={awayTeamNames} />
             <Grid className={classes.topRow}>
-                <Grid className={classes.story1}>
-                    <div className={`${classes.nameRow} ${classes.nameRowBig}`}>
-                        <div className={classes.leftPlayer}>
-                            <Avatar className={classes.avatarStyle} src={ava1}></Avatar>
-                            <div className={classes.playerName}>{student.username}</div>
-                        </div>
-                        <div className={classes.rightPlayer}>
-                            <div className={classes.playerName}>
-                                {student.storyOpponent.username}
-                            </div>
-                            <Avatar className={classes.avatarStyle} src={ava3}></Avatar>
-                        </div>
-                    </div>
-                    <div className={classes.subRow}>
-                        {/* High story1 */}
-                        <Grid item xs={12} sm={12} md={6}>
-                            <SubmissionDisplay
-                                key='story1Points'
-                                username={student.username}
-                                submission={student.story.page1}
-                                // points={state.story1Points}
-                                points={student.storyPoints}
-                                type='Story'
-                            />
-                        </Grid>
-                        <div className={classes.totalScoreBig}>
-                            <p>{student.storyTotal}</p> {/* High story1 + high story2 pts */}
-                        </div>
-                        {/* High story2 */}
-                        <Grid item xs={12} sm={12} md={6}>
-                            <SubmissionDisplay
-                                key='story1Points'
-                                // points={state.story1Points}
-                                username={student.storyOpponent.username}
-                                submission={student.storyOpponent.story.page1}
-                                points={student.storyOpponent.storyPoints}
-                                type='Story'
-                            />
-                        </Grid>
-                    </div>
-                    <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />
-                </Grid>
+                {/* STORY 1 AND STORY 2 */}
                 <Grid className={classes.story2}>
                     <div className={`${classes.nameRow} ${classes.nameRowSmall}`}>
                         <div className={classes.leftPlayer}>

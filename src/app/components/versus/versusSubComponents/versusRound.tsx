77d6c427fc89f5d3440688e32e7c4e559bdbb1b5
nicbongo <nicbongo@protonmail.com>
2020-04-18 18:40:52 -0300

    versus grid done, not buttons

--- src/app/components/versus/versusSubComponents/versusRound.tsx
+++ src/app/components/versus/versusSubComponents/versusRound.tsx
@@ -22,55 +22,71 @@ import {
     Grow,
     ClickAwayListener,
 } from '@material-ui/core';
+import { SubmissionDisplay } from '../modals/subDisplay.component';
+import vsImg from '../img/VS.png';
 import { useStyles } from '../versus-styles';
 
 interface RoundProps {
-    homePlayer: {};
+    roundStyle: {};
+    nameRowStyle: {};
     homeName: string;
-    awayPlayer: {};
+    homeAvatar: {};
+    homeSubmission: {};
+    homePoints: number;
+    awayName: string;
+    awayAvatar: {};
+    awaySubmission: {};
+    awayPoints: number;
 }
 
-const VersusRound: React.FC<RoundProps> = ({ homePlayer, awayPlayer }) => {
+const VersusRound: React.FC<RoundProps> = ({
+    roundStyle,
+    nameRowStyle,
+    homeName,
+    homeAvatar,
+    homeSubmission,
+    homePoints,
+    awayName,
+    awayAvatar,
+    awaySubmission,
+    awayPoints,
+}) => {
     const classes = useStyles();
     return (
-        <Grid className={classes.story1}>
-            <div className={`${classes.nameRow} ${classes.nameRowBig}`}>
+        <Grid className={`${roundStyle}`}>
+            <div className={`${classes.nameRow} ${nameRowStyle}`}>
                 <div className={classes.leftPlayer}>
-                    <Avatar
-                        className={classes.avatarStyle}
-                        src={homeplayer.player.avatar | awayPlayer.player.avatar}></Avatar>
-                    <div className={classes.playerName}>{student.username}</div>
+                    <Avatar className={classes.avatarStyle} src={'homeAvatar'}></Avatar>
+                    <div className={classes.playerName}>{homeName}</div>
                 </div>
                 <div className={classes.rightPlayer}>
-                    <div className={classes.playerName}>{student.storyOpponent.username}</div>
-                    <Avatar className={classes.avatarStyle} src={ava}></Avatar>
+                    <div className={classes.playerName}>{awayName}</div>
+                    <Avatar className={classes.avatarStyle} src={'awayAvatar'}></Avatar>
                 </div>
             </div>
             <div className={classes.subRow}>
                 {/* High story1 */}
                 <Grid item xs={12} sm={12} md={6}>
-                    {/* <SubmissionDisplay
+                    <SubmissionDisplay
                         key='story1Points'
                         username={homeName}
-                        submission={homePlayer.player.story.page1}
-                        // points={state.story1Points}
-                        points={homePlayer.player.storyPoints}
+                        submission={'homeSubmission'}
+                        points={homePoints}
                         type='Story'
-                    /> */}
+                    />
                 </Grid>
                 <div className={classes.totalScoreBig}>
-                    <p>{student.storyTotal}</p> {/* High story1 + high story2 pts */}
+                    <p>{homePoints + awayPoints}</p> {/* High story1 + high story2 pts */}
                 </div>
                 {/* High story2 */}
                 <Grid item xs={12} sm={12} md={6}>
-                    {/* <SubmissionDisplay
+                    <SubmissionDisplay
                         key='story1Points'
-                        // points={state.story1Points}
-                        username={student.storyOpponent.username}
-                        submission={student.storyOpponent.story.page1}
-                        points={student.storyOpponent.storyPoints}
+                        username={awayName}
+                        submission={'awaySubmission'}
+                        points={awayPoints}
                         type='Story'
-                    /> */}
+                    />
                 </Grid>
             </div>
             <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />

6afacb74c22fe48dc1d9a6a1f7717a9cd5207920
Chance Rutledge <chancerutledge2@gmail.com>
2020-04-21 16:33:16 -0700

    Emoji component wireframed in progress
    Co-authored-by: andrew maddocks <andrewmadd@icloud.com>
    Co-authored-by: Leo Sanchez <dothamaph@gmail.com>
    Co-authored-by: octanym <cmrnmr@gmail.com>

--- src/app/components/voting/emoji/Emoji.component.tsx
+++ src/app/components/voting/emoji/Emoji.component.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 
 const emojiSelection = [
     '😀',
@@ -7,23 +8,23 @@ const emojiSelection = [
     '😁',
     '😆',
     '😅',
-    'rolling laugh',
+    // 'rolling laugh',
     '😂',
     '🙂',
     '🙃',
     '😉',
     '😇',
-    'star eyes',
+    // 'star eyes',
     '😋',
     '😜',
-    'crazy eyes',
+    // 'crazy eyes',
     '😝',
-    'money tongue',
-    'covering mouth',
-    'shush',
-    'thinking',
-    'zipper mouth',
-    'raised eyebrow',
+    // 'money tongue',
+    // 'covering mouth',
+    // 'shush',
+    // 'thinking',
+    // 'zipper mouth',
+    // 'raised eyebrow',
     '😐',
     '😑',
     '😶',
@@ -31,40 +32,40 @@ const emojiSelection = [
     '😒',
     '🙄',
     '😬',
-    'pinocchio',
-    'smug smile',
+    // 'pinocchio',
+    // 'smug smile',
     '😞',
     '😪',
-    'drool',
+    // 'drool',
     '😴',
     '😷',
-    'thermometer',
-    'bandage',
-    'green face',
-    'vomit',
-    'nose blowing',
-    'red hot face',
-    'cold blue face',
-    'whirly smile',
-    'crossed out eyes',
-    'mind blown',
-    'cowboy hat',
-    'party',
+    // 'thermometer',
+    // 'bandage',
+    // 'green face',
+    // 'vomit',
+    // 'nose blowing',
+    // 'red hot face',
+    // 'cold blue face',
+    // 'whirly smile',
+    // 'crossed out eyes',
+    // 'mind blown',
+    // 'cowboy hat',
+    // 'party',
     '😎',
-    'nerdy glasses',
-    'monacle',
+    // 'nerdy glasses',
+    // 'monacle',
     '😕',
     '😟',
     '🙁',
-    'extra sad',
+    // 'extra sad',
     '😮',
     '😯',
     '😲',
     '😳',
-    'puppy eyes',
+    // 'puppy eyes',
     '😦',
     '😧',
-    'blue top frown',
+    // 'blue top frown',
     '😰',
     '😥',
     '😢',
@@ -72,14 +73,61 @@ const emojiSelection = [
     '😱',
     '😖',
     '😣',
-    'another sad',
-    'sweaty sad',
+    // 'another sad',
+    // 'sweaty sad',
     '😩',
     '😫',
-    'yawn with hand',
+    // 'yawn with hand',
     '😤',
 ];
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        emojiContainer: {
+            'display': 'flex',
+            'alignItems': 'center',
+            'width': '300px',
+            'height': '110px',
+            'justifyContent': 'space-evenly',
+            'flexWrap': 'wrap',
+            'overflowY': 'auto',
+            'margin': '1%',
+            'position': 'relative',
+            '&::-webkit-scrollbar': {
+                width: '.25em',
+            },
+            '&::-webkit-scrollbar-track': {
+                '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+            },
+            '&::-webkit-scrollbar-thumb': {
+                backgroundColor: 'rgba(0,0,0,.2)',
+                outline: '1px solid slategrey',
+                borderRadius: '10px',
+            },
+            // '&::-webkit-scrollbar-button': {
+            //     backgroundColor: 'rgba(0,0,0,.2)',
+            //     backgroundImage: `url('https://i.imgur.com/bj8uVqJ.jpg')`,
+            // },
+        },
+        emojiDiv: {
+            width: '30px',
+            height: '40px',
+            fontSize: '20px',
+        },
+    })
+);
+
 export const Emoji: React.FC = () => {
-    return <h1>Hello World</h1>;
+    const classes = useStyles({});
+    return (
+        <div className={classes.emojiContainer}>
+            {emojiSelection.map((emoji) => {
+                return (
+                    <div className={classes.emojiDiv} key={emoji}>
+                        {emoji}
+                    </div>
+                );
+            })}
+        </div>
+    );
 };

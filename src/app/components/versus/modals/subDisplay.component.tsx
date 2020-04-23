689bbd4c48a39f738c10fcb4f19beca870ea102c
Cameron Moore <cmrnmr@gmail.com>
2020-04-23 12:05:04 -0400

    edited versus modal for conditional render

--- src/app/components/versus/modals/subDisplay.component.tsx
+++ src/app/components/versus/modals/subDisplay.component.tsx
@@ -6,7 +6,6 @@ import { TextValidator } from 'react-material-ui-form-validator';
 import { TransitionProps } from '@material-ui/core/transitions';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
-import { ReactComponent as LockIcon } from '../img/lock-icon.svg';
 const Transition = React.forwardRef(function Transition(
     props: TransitionProps & { children?: React.ReactElement }
 ) {
@@ -55,7 +54,6 @@ export const SubmissionDisplay: React.FC<SubDisplayProps> = ({
     key,
     points,
 }) => {
-    const [isLocked, setLocked] = useState({ one: false, two: false, three: false, four: false });
     const [open, setOpen] = useState(false);
     const classes = useStyles({});
     const handleOpen = () => {
@@ -66,7 +64,7 @@ export const SubmissionDisplay: React.FC<SubDisplayProps> = ({
         setOpen(false);
     };
 
-    return !isLocked.one ? (
+    return (
         <>
             <Grid
                 container
@@ -81,44 +79,6 @@ export const SubmissionDisplay: React.FC<SubDisplayProps> = ({
                         onClick={handleOpen}
                         alt={`${username}'s ${type}`}
                     />
-                </Grid>
-                <Dialog fullScreen open={open}>
-                    <IconButton
-                        edge='start'
-                        color='inherit'
-                        onClick={handleClose}
-                        aria-label='close'>
-                        <CloseIcon />
-                    </IconButton>
-                    <DialogTitle id='submission-title'>{`${username}'s ${type}`}</DialogTitle>
-                    <div>
-                        <img src={submission} alt={`${username}'s ${type}`} />
-                    </div>
-                </Dialog>
-            </Grid>
-        </>
-    ) : (
-        <>
-            <Grid
-                container
-                direction='column'
-                justify='space-between'
-                alignItems='center'
-                alignContent='center'>
-                <Grid item md>
-                    <div
-                        style={{
-                            backgroundImage: `url(${submission})`,
-                            width: '180px',
-                            height: '120px',
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundSize: 'center',
-                        }}>
-                        <LockIcon />
-                    </div>
                 </Grid>
                 <Dialog fullScreen open={open}>
                     <IconButton
--- src/app/components/versus/versus.component.tsx
+++ src/app/components/versus/versus.component.tsx
@@ -32,6 +32,7 @@ import { Child } from '../../models';
 import { Link } from 'react-router-dom';
 import vsImg from './img/VS.png';
 import { SubmissionDisplay } from './modals/subDisplay.component';
+import { ReactComponent as LockedIcon } from './img/lock-icon.svg';
 import { useAPI } from '../../hooks';
 import { VersusHeader } from './versusSubComponents/versusHeader';
 interface VersusProps {
@@ -120,10 +121,11 @@ const Versus: React.FC<VersusProps> = ({ thisBattle }) => {
         }
     }, [response]);
     console.log(`response`, response?.battleInfo);
-
+    const [isLocked, setLocked] = useState({ oneVote: true, twoVotes: false, threeVotes: false });
     const homeTeamNames = `${student.username} & ${teammate.username}!`;
     const awayTeamNames = `${student.storyOpponent.username} & ${teammate.storyOpponent.username}`;
-    return (
+
+    return isLocked.oneVote ? (
         <Container className={classes.containerStyling}>
             <VersusHeader homeTeam={homeTeamNames} awayTeam={awayTeamNames} />
             <Grid className={classes.topRow}>
@@ -293,6 +295,169 @@ const Versus: React.FC<VersusProps> = ({ thisBattle }) => {
                                 points={teammate.illustrationOpponent.illustrationPoints}
                                 type='Illustration'
                             />
+                        </Grid>
+                    </div>
+                    <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />
+                </Grid>
+                {/*Buttons */}
+                <Grid className={classes.btnContainer}>
+                    <Grid className={classes.btnDiv}>
+                        <Link to={`/kids-dashboard/team-join`}>
+                            <Button className={classes.orangeButton} type='button'>
+                                Back
+                            </Button>
+                        </Link>
+                        <Link to={`/voting`}>
+                            <Button className={classes.orangeButton} type='submit'>
+                                Vote
+                            </Button>
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Container>
+    ) : (
+        <Container className={classes.containerStyling}>
+            <VersusHeader homeTeam={homeTeamNames} awayTeam={awayTeamNames} />
+            <Grid className={classes.topRow}>
+                <Grid className={classes.story1}>
+                    <div className={`${classes.nameRow} ${classes.nameRowBig}`}>
+                        <div className={classes.leftPlayer}>
+                            <Avatar className={classes.avatarStyle} src={ava1}></Avatar>
+                            <div className={classes.playerName}>{student.username}</div>
+                        </div>
+                        <div className={classes.rightPlayer}>
+                            <div className={classes.playerName}>
+                                {student.storyOpponent.username}
+                            </div>
+                            <Avatar className={classes.avatarStyle} src={ava3}></Avatar>
+                        </div>
+                    </div>
+                    <div className={classes.subRow}>
+                        {/* High story1 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <SubmissionDisplay
+                                key='story1Points'
+                                username={student.username}
+                                submission={student.story.page1}
+                                // points={state.story1Points}
+                                points={student.storyPoints}
+                                type='Story'
+                            />
+                        </Grid>
+                        <div className={classes.totalScoreBig}>
+                            <p>{student.storyTotal}</p> {/* High story1 + high story2 pts */}
+                        </div>
+                        {/* High story2 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <LockedIcon />
+                        </Grid>
+                    </div>
+                    <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />
+                </Grid>
+                <Grid className={classes.story2}>
+                    <div className={`${classes.nameRow} ${classes.nameRowSmall}`}>
+                        <div className={classes.leftPlayer}>
+                            <Avatar className={classes.avatarStyle} src={ava2}></Avatar>
+                            <div className={classes.playerName}>{teammate.username}</div>
+                        </div>
+                        <div className={classes.rightPlayer}>
+                            <div className={classes.playerName}>
+                                {teammate.storyOpponent.username}
+                            </div>
+                            <Avatar className={classes.avatarStyle} src={ava4}></Avatar>
+                        </div>
+                    </div>
+                    <div className={classes.subRow}>
+                        {/* Low story1 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <SubmissionDisplay
+                                key='story1Points'
+                                username={teammate.username}
+                                submission={teammate.story.page1}
+                                // points={state.story1Points}
+                                points={teammate.storyPoints}
+                                type='Story'
+                            />
+                        </Grid>
+                        <div className={classes.totalScoreSmall}>
+                            <p>{teammate.storyTotal}</p> {/* low story1 + low story2 pts */}
+                        </div>
+                        {/* Low story2 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <LockedIcon />
+                        </Grid>
+                    </div>
+                    <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />
+                </Grid>
+            </Grid>
+            <Grid className={classes.bottomRow}>
+                <Grid className={classes.picture1}>
+                    <div className={`${classes.nameRow} ${classes.nameRowSmall}`}>
+                        <div className={classes.leftPlayer}>
+                            <Avatar className={classes.avatarStyle} src={ava1}></Avatar>
+                            <div className={classes.playerName}>{student.username}</div>
+                        </div>
+                        <div className={classes.rightPlayer}>
+                            <div className={classes.playerName}>
+                                {student.illustrationOpponent.username}
+                            </div>
+                            <Avatar className={classes.avatarStyle} src={ava3}></Avatar>
+                        </div>
+                    </div>
+                    <div className={classes.subRow}>
+                        {/* high pic1 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <SubmissionDisplay
+                                key='pic1Points'
+                                username={student.username}
+                                submission={student.illustration}
+                                // points={state.story1Points}
+                                points={student.illustrationPoints}
+                                type='Illustration'
+                            />
+                        </Grid>
+                        <div className={classes.totalScoreSmall}>
+                            <p>{student.illustrationTotal}</p> {/* High pic1 + high pic2 pts */}
+                        </div>
+                        {/* high pic2 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <LockedIcon />
+                        </Grid>
+                    </div>
+                    <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />
+                </Grid>
+                <Grid className={classes.picture2}>
+                    <div className={`${classes.nameRow} ${classes.nameRowBig}`}>
+                        <div className={classes.leftPlayer}>
+                            <Avatar className={classes.avatarStyle} src={ava2}></Avatar>
+                            <div className={classes.playerName}>{teammate.username}</div>
+                        </div>
+                        <div className={classes.rightPlayer}>
+                            <div className={classes.playerName}>
+                                {teammate.illustrationOpponent.username}
+                            </div>
+                            <Avatar className={classes.avatarStyle} src={ava4}></Avatar>
+                        </div>
+                    </div>
+                    <div className={classes.subRow}>
+                        {/* Low pic1 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <SubmissionDisplay
+                                key='pic1Points'
+                                username={teammate.username}
+                                submission={teammate.illustration}
+                                // points={state.story1Points}
+                                points={teammate.illustrationPoints}
+                                type='Illustration'
+                            />
+                        </Grid>
+                        <div className={classes.totalScoreBig}>
+                            <p>{teammate.illustrationTotal}</p> {/* low pic1 + low pic2 pts */}
+                        </div>
+                        {/* Low Pic2 */}
+                        <Grid item xs={12} sm={12} md={6}>
+                            <LockedIcon />
                         </Grid>
                     </div>
                     <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />

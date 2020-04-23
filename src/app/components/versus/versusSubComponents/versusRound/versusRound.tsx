7e36e289408944905ebeb6f317ebac8a39bfcab6
Cameron Moore <cmrnmr@gmail.com>
2020-04-23 19:15:11 -0400

    started lock setup on vsround

--- src/app/components/versus/versusSubComponents/versusRound/versusRound.tsx
+++ src/app/components/versus/versusSubComponents/versusRound/versusRound.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, Grid } from '@material-ui/core';
 import { SubmissionDisplay } from '../../modals/subDisplay.component';
 import { useStyles } from '../../versus-styles';
 import vsImg from '../../img/VS.png';
+import { ReactComponent as LockIcon } from '../../img/lock-icon.svg';
 
 interface RoundProps {
     roundStyle: {};
@@ -30,10 +31,11 @@ const VersusRound: React.FC<RoundProps> = ({
     awayPoints,
 }) => {
     const classes = useStyles();
+    const [locked, setLocked] = useState(true);
     const submissionCheck = (submission) =>
         typeof submission === 'string' ? submission : Object.values(submission);
     console.log(`asdasd`, nameRowStyle);
-    return (
+    return !locked ? (
         <Grid className={`${roundStyle}`}>
             <div className={`${classes.nameRow} ${nameRowStyle}`}>
                 <div className={classes.leftPlayer}>
@@ -67,6 +69,41 @@ const VersusRound: React.FC<RoundProps> = ({
                         username={awayName}
                         submission={submissionCheck(awaySubmission)}
                     />
+                </Grid>
+            </div>
+            <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />
+        </Grid>
+    ) : (
+        <Grid className={`${roundStyle}`}>
+            <div className={`${classes.nameRow} ${nameRowStyle}`}>
+                <div className={classes.leftPlayer}>
+                    <Avatar className={classes.avatarStyle} src={homeAvatar}></Avatar>
+                    <div className={classes.playerName}>{homeName}</div>
+                </div>
+                <div className={classes.rightPlayer}>
+                    <div className={classes.playerName}>{awayName}</div>
+                    <Avatar className={classes.avatarStyle} src={awayAvatar}></Avatar>
+                </div>
+            </div>
+            <div className={classes.subRow}>
+                {/* High story1 */}
+                <Grid item xs={12} sm={12} md={6}>
+                    <SubmissionDisplay
+                        username={homeName}
+                        submission={submissionCheck(homeSubmission)}
+                    />
+                </Grid>
+                {nameRowStyle === classes.nameRowBig ? (
+                    <div className={classes.totalScoreBig}>
+                        <p>{homePoints + awayPoints}</p>
+                    </div>
+                ) : (
+                    <div className={classes.totalScoreSmall}>
+                        <p>{homePoints + awayPoints}</p>
+                    </div>
+                )}
+                <Grid item xs={12} sm={12} md={6}>
+                    <LockIcon />
                 </Grid>
             </div>
             <img className={classes.vs} src={vsImg} alt='vs lightning bolt' />

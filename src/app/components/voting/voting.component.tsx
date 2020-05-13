430879de1ebebfc439db90849065c2c37ebbf6ce
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-05-13 14:05:29 -0400

    fixes issues with voting functionality and reroute
    Co-authored-by:
     andrew maddocks <andrewmadd@icloud.com(opens in new tab)>
    octanym <cmrnmr@gmail.com(opens in new tab)>
    lgv-0 <loganvan@att.net(opens in new tab)>
    evrgreen <rcarsten06@gmail.com(opens in new tab)>
    crutledgedev <chancerutledge2@gmail.com(opens in new tab)>

--- src/app/components/voting/voting.component.tsx
+++ src/app/components/voting/voting.component.tsx
@@ -6,8 +6,6 @@ import { Button, Typography, Container, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import Radio, { RadioProps } from '@material-ui/core/Radio';
 import { Child, Cohort } from '../../models';
-// import story1 from './img/leowriting.jpg';
-// import story2 from './img/chancewriting.jpg';
 import { useForm } from '../../hooks';
 import { VotingModal } from './modal/modal-image';
 import { Emoji } from './emoji/Emoji.component';
@@ -33,16 +31,10 @@ interface VotingCardProps {
 }
 
 const Voting: React.FC<VotingCardProps> = ({ child }) => {
+    const history = useHistory();
     const [response] = useAPI(`/votingRoutes/voting`, 'GET', false);
     const [postResponse, loading, request] = useAPI(`/votingRoutes/voting`, 'POST');
     console.log('response', response);
-    // const { state, handleInputChange, handleSubmitBuilder } = useForm({
-    //     story1Points: 'something',
-    //     story2Points: 'something',
-    //     pic1Points: 'something',
-    //     pic2Points: 'something',
-    // });
-
     const [dummyData, setDummyData] = useState({ dummy: 'data' });
     const [selectedValue, setSelectedValue] = React.useState<number>();
     const [open, setOpen] = React.useState(false);
@@ -65,19 +57,20 @@ const Voting: React.FC<VotingCardProps> = ({ child }) => {
     };
 
     const handleSubmit = () => {
-        const vote = { matchupID: response?.matchupID, childID: selectedValue };
+        const emojiObj = {};
+        emojiObj[response?.child1.childId] = newEmoji.player1;
+        emojiObj[response?.child2.childId] = newEmoji.player2;
+        const vote = { matchupID: response?.matchupID, childID: selectedValue, emojiObj };
         console.log('vote data', vote);
         if (newEmoji.player1.length < 4 || newEmoji.player2.length < 4) {
             setOpen(true);
         } else {
             console.log('submitted successfully');
             request(vote);
+            history.push('/matchup');
         }
         // history.push
     };
-
-    // console.log('this is the object', newEmoji);
-    // console.log('selected value', selectedValue);
 
     return (
         <Container className={classes.containerStyling}>
@@ -85,24 +78,21 @@ const Voting: React.FC<VotingCardProps> = ({ child }) => {
             <Grid className={classes.topRow}>
                 <Grid className={classes.story1}>
                     <div className={classes.playerSelection}>
-                        <p>{'ID:' + response?.child1.id}</p>
                         <VotingModal
                             key='pic1Points'
-                            // username={dummyData.dummy}
                             submission={
                                 response?.child1.story
                                     ? response?.child1.story.page1
                                     : response?.child1.illustration
                             }
                             type='Illustration'
-                            // points={state.pic1Points}
                             response={response}
                         />
                         <ColoredRadio
                             className={classes.radioBox}
-                            checked={selectedValue == response?.child1.id}
+                            checked={selectedValue == response?.child1.childId}
                             onChange={handleChange}
-                            value={response?.child1.id}
+                            value={response?.child1.childId}
                             name='storyA'
                             inputProps={{ 'aria-label': 'A' }}
                             disableRipple={true}
@@ -116,24 +106,21 @@ const Voting: React.FC<VotingCardProps> = ({ child }) => {
                 </Grid>
                 <Grid className={classes.story2}>
                     <div className={classes.playerSelection}>
-                        <p>{'ID:' + response?.child2.id}</p>
                         <VotingModal
                             key='pic1Points'
-                            // username={dummyData.dummy}
                             submission={
                                 response?.child2.story
                                     ? response?.child2.story.page1
                                     : response?.child2.illustration
                             }
                             type='Illustration'
-                            // points={state.pic1Points}
                             response={response}
                         />
                         <ColoredRadio
                             className={classes.radioBox}
-                            checked={selectedValue == response?.child2.id}
+                            checked={selectedValue == response?.child2.childId}
                             onChange={handleChange}
-                            value={response?.child2.id}
+                            value={response?.child2.childId}
                             name='storyB'
                             inputProps={{ 'aria-label': 'B' }}
                             disableRipple={true}

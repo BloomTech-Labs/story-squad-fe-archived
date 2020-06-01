7ea8698df46eb03f22773140947bf93bccf542a3
JGPico <minq_2@hotmail.com>
2020-06-01 13:45:04 -0600

    Can view picture in moderator page

--- src/app/components/admin-dashboard/cohort/submission/drawing.submission.tsx
+++ src/app/components/admin-dashboard/cohort/submission/drawing.submission.tsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAPI } from '../../../../hooks';
 
+import { Button } from '@material-ui/core';
+
 // http://localhost:3000/admin/dashboard/cohort/:child_id/details/drawing
+
+interface Illustration {
+    childId: number;
+    id: number;
+    illustration: string;
+    points: number;
+    votes: number;
+    week: number;
+}
 
 const DrawingSubmissions: React.FC = () => {
     const { id } = useParams();
-    return <div>Hello it's me drawings</div>;
+    const [response, loading, request] = useAPI(`illustrationRoutes/children/${id}/`);
+    const [illustration, setIllustration] = useState<Illustration>();
+    console.log('Drawing response', response);
+
+    useEffect(() => {
+        if (response !== undefined) {
+            setIllustration(response.illustrations[0]);
+        }
+    }, [response]);
+
+    return (
+        <>
+            <div>
+                <img
+                    style={{ maxWidth: '600px' }}
+                    src={illustration && illustration.illustration}
+                    alt=''
+                />
+            </div>
+            <div>
+                <Button color='primary' variant='contained'>
+                    Dud button
+                </Button>
+            </div>
+        </>
+    );
 };
 
 export { DrawingSubmissions };
